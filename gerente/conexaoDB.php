3c1194701fb55db493e2db45173f880dbc304e3b
Vitor Gabriel <vitor.figueiredo.136@ufrn.edu.br>
2026-09-09 17:15:32 -0300

    Update database password environment variable

--- gerente/conexaoDB.php
+++ gerente/conexaoDB.php
@@ -1,7 +1,7 @@
 <?php
 
 $usuarioBanco = 'root';
-$senhaBanco = getenv('BancoGoogle!1');
+$senhaBanco = getenv('DB_PASSWORD');
 $nomeBanco = 'farmacerta';
 
 $socket = '/cloudsql/farmacia-508118:europe-west1:farmacerta';
